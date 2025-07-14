f27d81b7dac9158b20506a5392b3afb778999f34
Asebs047 <eherrera-2024047@kinal.edu.gt>
2025-07-13 22:49:01 -0600

    initial commit

---
+++ src/main/java/org/stepone/system/Main.java
@@ -0,0 +1,16 @@
+package org.stepone.system;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author emili
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
